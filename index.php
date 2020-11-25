37007d25b9456fb4ed465a56fc498c0b717f0d5c
Jacob Mziya <jacobmziya@gmail.com>
2020-11-25 23:34:29 +0200

    Created initial image search form in the homepage.

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php ?>
 
 <html lang="en">
-<h1>Chithunzi.com Initial</h1>
+<h1>Chithunzi</h1>
+
+<form>
+    <label for="image_search"></label>
+    <input id="image_search" name="text" placeholder="Search for an image by keywords (e.g. 'malawian dog','mzuzu coffee')">
+
+    <button type="submit">Search</button>
+</form>

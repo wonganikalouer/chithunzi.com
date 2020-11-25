c7370b3ad0dc0d3a63dd6c78d385a9533bc9c200
Jacob Mziya <jacobmziya@gmail.com>
2020-11-25 23:56:36 +0200

    Added a Buy Us Coffee widget button on the homepage.

--- index.php
+++ index.php
@@ -9,3 +9,6 @@
 
     <button type="submit">Search</button>
 </form>
+
+<!-- TODO: To create a DivTag Paypal Merchant Account to be receiving donations and buy me a coffee payments. -->
+<script data-name="BMC-Widget" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="chithunzi" data-description="Support me on Buy me a coffee!" data-message="Thank you for visiting. You can also buy us a coffee!" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
